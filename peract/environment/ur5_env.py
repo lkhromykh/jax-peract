e218f046f3d62d9d1c9a67d36d2d4e648e3fe725
lkhromykh <leonidgm@gmail.com>
2024-05-08 18:21:07 +0300

    updating to ROS representation

--- peract/environment/ur5_env.py
+++ peract/environment/ur5_env.py
@@ -1,6 +1,6 @@
 import dm_env
 import numpy as np
-from scipy.spatial.transform import Rotation as R
+from scipy.spatial.transform import Rotation
 from ur_env.remote import RemoteEnvClient
 
 from peract.environment import gcenv
@@ -25,7 +25,7 @@ class UREnv(gcenv.GoalConditionedEnv):
 
     def step(self, action) -> dm_env.TimeStep:
         pos, euler, other = np.split(action, [3, 6])
-        rotvec = R.from_euler('ZYX', euler).as_rotvec()
+        rotvec = Rotation.from_euler('ZYX', euler).as_rotvec()
         action = np.r_[pos, rotvec, other].astype(np.float32)
         ts = self._env.step(action)
         self._step += 1
@@ -37,8 +37,9 @@ class UREnv(gcenv.GoalConditionedEnv):
 
     @staticmethod
     def extract_observation(obs: dict[str, np.ndarray]) -> gcenv.Observation:
-        pos, rotvec = np.split(obs['tcp_pose'], [3])
-        euler = R.from_rotvec(rotvec).as_euler('ZYX')
+        pos, quat = np.split(obs['tcp_pose'], [3])
+        euler = Rotation.from_quat(quat).as_euler('ZYX')
+        tcp_pose = np.r_[pos, euler]
         def rot_kinect(x): return np.fliplr(np.swapaxes(x, 0, 1)),
         return gcenv.Observation(
             images=rot_kinect(obs['image']),
@@ -46,7 +47,7 @@ class UREnv(gcenv.GoalConditionedEnv):
             point_clouds=rot_kinect(obs['point_cloud']),
             joint_positions=np.asarray(obs['joint_position']),
             joint_velocities=np.asarray(obs['joint_velocity']),
-            tcp_pose=np.r_[pos, euler],
+            tcp_pose=tcp_pose,
             gripper_pos=obs['gripper_pos'],
             gripper_is_obj_detected=obs['gripper_is_obj_detected'],
             is_terminal=obs['is_terminal'],
